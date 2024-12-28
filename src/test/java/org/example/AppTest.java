38d9499eeffcc7d189beda66f5c2c76ffb2c3828
DaniilBrylev <dan.brylev38@gmail.ru>
2024-12-29 00:24:33 +0300

    1

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class AppTest {
+    @Test
+    public void simpleTest() {
+        Assert.assertEquals(2 + 2, 4);
+    }
+}
